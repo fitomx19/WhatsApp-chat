078e9d8b2dbdcd614bb2681b507ffd24f9bd8b29
Michael Adolfo Huerta <michael.huerta@getechnologies.com.mx>
2023-11-27 13:07:53 -0600

    deploy heroku

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 
 // Configurar las rutas
--- routes.js
+++ routes.js
@@ -1,4 +1,3 @@
-// routes.js
 const express = require('express');
 const controller = require('./controller');
 
