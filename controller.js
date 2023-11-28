0787c05942400820f6dee71d76217aa2fbf78af3
Michael Adolfo Huerta <michael.huerta@getechnologies.com.mx>
2023-11-27 22:25:22 -0600

    avance

--- controller.js
+++ controller.js
@@ -14,41 +14,42 @@ const handleWebhookGet = (req, res) => {
   }
 };
 
-const handleWebhookPost = (req, res) => {
-  const data = req.body;
 
+const handleWebhookPost = async (req, res) => {
+  const data = req.body;
   const telefonoCliente = data.entry[0].changes[0].value.messages[0].from;
   const mensaje = data.entry[0].changes[0].value.messages[0].text.body;
   const idWA = data.entry[0].changes[0].value.messages[0].id;
   const timestamp = data.entry[0].changes[0].value.messages[0].timestamp;
 
-  if (mensaje) {
-
-    try{
-      var bot = new RiveScript();
-      bot.loadFile("restaurante.rive").then(loading_done).catch(loading_error);
+  if (data.mensaje) {
+    try {
+      const bot = new RiveScript();
+      // Cargar el archivo y esperar a que cargue
+      await new Promise((resolve, reject) => {
+        bot.loadFile("restaurante.rive", resolve, reject);
+      });
 
       bot.sortReplies();
-      username = "local-user";
+      const username = "local-user";
 
-      bot.reply(username, "Hello, bot!").then(function(reply) {
-        console.log("The bot says: " + reply);
-        fs.writeFileSync('texto.txt', "respuesta: " + reply + "timestamp: " + timestamp + "\n");
-      });
-    }catch(err){
+      const reply = await bot.reply(username, mensaje);
+
+      console.log("The bot says: " + reply);
+      fs.writeFileSync('texto.txt', "respuesta: " + reply + "timestamp: " + timestamp + "\n");
+
+      res.status(200).json({ status: 'success' });
+    } catch (err) {
       console.log(err);
+      res.status(500).json({ status: 'error', message: 'Error en el procesamiento del mensaje.' });
     }
-
-    console.log(`Número de teléfono: ${telefonoCliente}`);
-    console.log(`Mensaje: ${mensaje}`);
-    console.log(`ID de WhatsApp: ${idWA}`);
-    console.log(`Timestamp: ${timestamp}`);
-
-    res.status(200).json({ status: 'success' });
   } else {
     res.status(400).json({ status: 'error', message: 'No hay mensaje recibido.' });
   }
 };
+
+
+
 
 const handleTextoGet = (req, res) => {
   const texto = fs.readFileSync('texto.txt', 'utf8');
